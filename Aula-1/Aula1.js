2ad7c2039ad531a65dad91d4c450610f8e2a95a8
guilhermeneal <guilhermeneal@gmail.com>
2026-02-21 09:33:50 +0000

    Add files via upload

--- Aula-1/Aula1.js
+++ Aula-1/Aula1.js
@@ -1,47 +1,47 @@
 function CalculateGrade(p1,p2,f){
-    return (p1 * 0.3) + (p2 * 0.3) + (f * 0.4);
+    console.log (p1 * 0.3) + (p2 * 0.3) + (f * 0.4);
 }
 var finalGrade = CalculateGrade(12, 15, 13);
 if (finalGrade >= 10):
-    console.log("Aprovado");else:
-    console.log("Reprovado");
+    console.log((Aprovado();else:
+    console.log((Reprovado();
 
 function numeroMes(mes){
     if (mes == 1){
-        return "Janeiro";
+        console.log (Janeiro(;
     }    else if (mes == 2){
-        return "Fevereiro";
+        console.log (Fevereiro(;
     }    else if (mes == 3){
-        return "Março";
+        console.log (Março(;
     }    else if (mes == 4){
-        return "Abril";
+        console.log (Abril(;
     }    else if (mes == 5){
-        return "Maio";
+        console.log (Maio(;
     }    else if (mes == 6){
-        return "Junho";
+        console.log (Junho(;
     }    else if (mes == 7){
-        return "Julho";
+        console.log (Julho(;
     }   else if (mes == 8){
-        return "Agosto";
+        console.log (Agosto(;
     }   else if (mes == 9){
-        return "Setembro";
+        console.log (Setembro(;
     }    else if (mes == 10){
-        return "Outubro";
+        console.log (Outubro(;
     }    else if (mes == 11){
-        return "Novembro";
+        console.log (Novembro(;
     }    else if (mes == 12){
-        return "Dezembro";
+        console.log (Dezembro(;
 }
 console.log(numeroMes(5));
 
 function calculadora(operacao, num1, num2){
     if (operacao == "soma"){
-        return num1 + num2;
+        console.log num1 + num2;
     }    else if (operacao == "subtracao"){
-        return num1 - num2;
+        console.log num1 - num2;
     }    else if (operacao == "multiplicacao"){
-        return num1 * num2;
+        console.log num1 * num2;
     }    else if (operacao == "divisao"){
-        return num1 / num2;
+        console.log num1 / num2;
     }
 }
